5929ada4ca30e7b5042d42104a5b5964cdbf7dd2
Franklin <ftorrealba27444@columbushs.com>
2025-11-10 11:36:06 -0500

    delete

--- Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
+++ Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
@@ -3,24 +3,83 @@
 */
 
 // TODO 1: Create variables age (34) and price (12.99), print both
+
+let age = 34; 
+console.log(age);
+let price = 12.99;
+console.log(price);
+
 // TODO 1b: Divide "Sandra" by 2 and print result (expect NaN)
 
+let whatIsThis = "Sandra" / 2;
+console.log("WhatIsThis " + whatISThis)
+
 // TODO 2: Print basic math ops: + - * /
+console.log("2 + 2 =  ", 2 + 2)
+console.log("4 - 2 = ", 4 - 2)
+console.log("3 * 2 = ", 3 * 2)
+console.log("6 / 2 = ", 6 / 2)
+
 // TODO 2b: Use exponentiation (2 ** 3) and modulo (7 % 2)
 
+console.log("2 ** 3 = ", 2 ** 3)
+console.log("7 % 2 = ", 7 % 2)
+
 // TODO 3: Assignment operators with score variable
+let score = 10 
+console.log(score)
+score += 5
+console.log(score)
+score -= 2
+score *= 4
+score /= 2
+console.log(score)
 
 // TODO 4: Expressions with operator precedence
+let result = ((7+5) / 3) - 8;
+console.log("Result ", result)
+
+let result2 = 2 + 2 * 2 // 6
+let result3 = (2 + 2) * 2 // 8
+
 
 // TODO 5: Strings (single, double, backticks)
+console.log("Mr. Explorer")
+console.log("Mr. Explorer")
+console.log(`Mr. Explorer`)
+
+let name = "Mr. Explorer"
+let age2 = (2025 - 1958)
+
+console.log(`Hello, my name is ${name} and I am ${age2} year old`)
+
+
 
 // TODO 6: String properties and methods
+let greeting = "Hello there";
 // - greeting.length
+console.log("Greeting length: " + greeting.length)
 // - charAt(0), charAt(5), [3]
+console.log("CharAT (0)", greeting.charAt (0))
+console.log("ChartAt (5) ", greeting.charAt (5) ) 
+console.log("[3]", greeting[3])
 // - indexOf("sad")
+let phrase = "Don't be sad, be happy!"
+console.log("Index of sad", phrase.indexOf ("sad"))
 // - repeat()
+console.log("la".repeat(5))
 // - substring, substr, slice examples
-
+console.log("substring ", phrase.substring (0, 3))
+console.log("Substr () ", phrase.substr (0,3))
+console.log("Slice ", phrase.slice(-23, -20))
 // 🚀 Stretch Goals
 // - Even number check using %
+for(let i = 1; i < 101; i++) {
+    if (i % 2 == 0) {
+        console.log(`The answer ${i} is even`)
+    } else {
+        console.log(`The answer ${1} is odd`)
+    }
+}
 // - localeCompare() example
+console.log("apple".localeCompare("banana"))
--- Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.2/src/Lab1_2_DataTypes.js
+++ Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.2/src/Lab1_2_DataTypes.js
@@ -5,26 +5,47 @@
 
 // TODO 1: Declare one variable for each primitive type discussed in the PDF.
 // string, number, boolean, null, undefined
-let s; // = ?
-let n; // = ?
-let b; // = ?
-let z; // = null
-let u; // (leave undefined)
+let movie = "Bronx Tail"
+let rating = 6.8;
+let revenue = null;
+let oscars;
+let inTheaters = true;
+
+console.log("Movie", typeof movie)
+console.log("Rating", typeof rating)
+console.log("revenue", typeof revenue)
+console.log("oscar", typeof oscars)
+console.log("inTheaters", typeof inTheaters)
 
 // TODO 2: Use typeof to log each variable's type.
 // console.log(typeof s);
 
 // TODO 3: Show string concatenation vs numeric addition using "5" and 5.
+console.log("5" + 5)
+console.log(5 + 5  + "5")
+console.log("5" + (5 + 5))
 
 // TODO 4: Explicit conversions using Number(), String(), Boolean()
+console.log(Number("5"))
+console.log(Boolean(""))
+console.log(boolean("1"))
+console.log(typeof String(4))
 
 // TODO 5: NaN and isNaN: demonstrate a case where conversion fails.
+console.log(isNaN("hello"))
+
 
 // TODO 6: Template literals: print a line that mixes text + a variable value.
 
+console.log(`My favorite movie is ${movie} 
+   its rating on IMDB is ${rating} 
+   the movie has made ${revenue} 
+   at the box office and has won ${oscars} oscars 
+   and is currently in theaters: ${inTheaters}`)
+
 /* ===========================================================
    🎟 EXIT TICKET
-   1. Which data type(s) did you use today and why?
+   1. Which data type(s) did you use today and why? string, number, bollean, null 
    2. Where did you encounter type conversion (implicit or explicit)?
    3. One misconception I corrected or a tip I learned:
    4. What would you try next time to debug faster?
